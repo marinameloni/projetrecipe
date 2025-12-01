40d321a0f3358413db968c1330867c03768bdee7
paola <marina.meloni@edu.univ-fcomte.fr>
2025-12-01 16:25:36 +0100

    feat(api): implement recipe fetching and define Recipe type

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+const { data: recipes, error } = await useAsyncData("recipes", async () => {
+  const response = await $fetch<{ data: Recipe[] }>("http://localhost:4000/api/recipes");
+  return response.data;
+});
+
+if (error && error.value) throw new Error("Page not Found");
+definePageMeta({
+  layout: 'aside'
+})
+</script>
+
 <template>
   <div>
     <div class="o-recipeBlock">
@@ -10,8 +22,7 @@
           <img src="https://placeholder.pics/svg/600x400" alt="Delicious Chicken Wings">
         </div>
 
-        <button class="a-button -primary">
-          View recipes
+        <button class="a-button -primary">View Recipe
         </button>
       </div>
     </div>
@@ -19,11 +30,10 @@
     <h2>Categories</h2>
     <h3>Simple and fun recipes</h3>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    
+    <ul v-if="recipes && recipes.length">
+      <li v-for="(recipe, index) in recipes" :key="index">{{ recipe.title }}</li>
+    </ul>
   </div>
 </template>
 
-<script setup>
-definePageMeta({
-  layout: 'aside'
-})
-</script>
---
+++ app/types/api/recipe.d.ts
@@ -0,0 +1,10 @@
+type Recipe = {
+  recipe_id: number;
+  title: string;
+  description: string;
+  image_url: string;
+  cuisine_name: string;
+  goal_name: string;
+  diet_name: string;
+  allergy_name: string;
+};
